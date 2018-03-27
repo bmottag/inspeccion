63cac45380b4ba6cd2e80ab4b6781bf3053b76ea
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-03-27 06:55:10 -0600

    CLiente: Se ajusta validaciones, se ajusta formulario

--- application/modules/admin/views/form_cliente.php
+++ application/modules/admin/views/form_cliente.php
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="<?php echo base_url("assets/js/validate/admin/cliente.js"); ?>"></script>
+
 <div class="right_col" role="main">
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12">
@@ -7,50 +9,71 @@
 					<ul class="nav navbar-right panel_toolbox">
 						<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 						</li>
-						<li><a class="close-link"><i class="fa fa-close"></i></a>
-						</li>
 					</ul>
 					<div class="clearfix"></div>
 				</div>
 				<div class="x_content">
 				<br />
-				<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+					<form id="form" data-parsley-validate class="form-horizontal form-label-left">
 
-					<div class="form-group">
-						<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombres <span class="required">*</span>
-						</label>
-						<div class="col-md-6 col-sm-6 col-xs-12">
-							<input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12" maxlength=30>
+						<div class="form-group">
+							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="nombres">Nombres <span class="required">*</span>
+							</label>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<input type="text" id="nombres" name="nombres" required="required" class="form-control col-md-7 col-xs-12" maxlength=30 placeholder="Nombres">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Apellidos <span class="required">*</span>
-						</label>
-						<div class="col-md-6 col-sm-6 col-xs-12">
-							<input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" maxlength=30>
+						<div class="form-group">
+							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="apellidos">Apellidos <span class="required">*</span>
+							</label>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<input type="text" id="apellidos" name="apellidos" required="required" class="form-control col-md-7 col-xs-12" maxlength=30 placeholder="Apellidos">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Email <span class="required">*</span></label>
-						<div class="col-md-6 col-sm-6 col-xs-12">
-							<input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name" maxlength=100>
+						<div class="form-group">
+							<label for="email" class="control-label col-md-3 col-sm-3 col-xs-12">Email <span class="required">*</span></label>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<input id="email" name="email" class="form-control col-md-7 col-xs-12" type="text" maxlength=50 placeholder="Email">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">No. Celular <span class="required">*</span></label>
-						<div class="col-md-6 col-sm-6 col-xs-12">
-							<input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
+						<div class="form-group">
+							<label for="celular" class="control-label col-md-3 col-sm-3 col-xs-12">No. Celular <span class="required">*</span></label>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<input id="celular" name="celular" class="form-control col-md-7 col-xs-12" type="text" maxlength=12 placeholder="No. Celular">
+							</div>
 						</div>
-					</div>
 
-					<div class="ln_solid"></div>
-					<div class="form-group">
-						<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-							<button type="submit" class="btn btn-success">Guardar</button>
+						<div class="ln_solid"></div>
+						<div class="form-group">
+							<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+								<div class="row" align="center">
+									<div style="width:50%;" align="center">
+										<button type="button" id="btnSubmit" name="btnSubmit" class='btn btn-success'>
+												Guardar <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true">
+										</button>								
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+												
+						<div class="form-group">
+							<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+								
+								<div id="div_load" style="display:none">		
+									<div class="progress progress-striped active">
+										<div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+											<span class="sr-only">45% completado</span>
+										</div>
+									</div>
+								</div>
+								<div id="div_error" style="display:none">			
+									<div class="alert alert-danger"><span class="glyphicon glyphicon-remove" id="span_msj">&nbsp;</span></div>
+								</div>	
+								
+							</div>
+						</div>
 
-				</form>
+					</form>
 				</div>
 			</div>
 		</div>
